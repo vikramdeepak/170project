8911a2089c4107585c9ce95ad4299d603f3c0139
Vikram Deepak <vikram.deepak@berkeley.edu>
2021-05-01 20:17:36 -0700

    test

--- solver.py
+++ solver.py
@@ -41,9 +41,11 @@ def solve(G):
 
 # For testing a folder of inputs to create a folder of outputs, you can use glob (need to import it)
 if __name__ == '__main__':
-    inputs = glob.glob('inputs/small/*')
+    inputs = glob.glob('inputs/large/*')
+    
     for input_path in inputs:
-        output_path = 'outputs/small/' + basename(normpath(input_path))[:-3] + '.out'
+        print(input_path)
+        output_path = 'outputs/large/' + basename(normpath(input_path))[:-3] + '.out'
         G = read_input_file(input_path)
         c, k = solve(G)
         assert is_valid_solution(G, c, k)
